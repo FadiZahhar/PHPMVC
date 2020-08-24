49a59f4574907e63d6a9d735fb1b381b00441762
Debian <bitnami@ip-172-26-4-191.eu-central-1.compute.internal>
2020-08-24 13:08:25 +0000

    fixing the configuration of the example demo

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
   // DB Params
   define("DB_HOST", "localhost");
   define("DB_USER", "root");
-  define("DB_PASS", "123456");
-  define("DB_NAME", "traversymvc");
+  define("DB_PASS", "sRTsNSP815xc");
+  define("DB_NAME", "phpmvc");
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/shareposts');
+  define('URLROOT', 'http://phpmvc.domvp.xyz');
   // Site Name
-  define('SITENAME', 'SharePosts');
+  define('SITENAME', 'PHPMVC');
   
